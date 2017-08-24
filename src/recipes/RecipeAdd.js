b2fcb19c547bbb7dff793bf59897b04d6a98f6cc
elchrisso <elchrisso@hotmail.com>
2017-08-23 20:13:09 -0600

    Got RecipeAdd wrapped in graphql and addrecipe, and have it working from gui.  foodIds is hardcoded, next step is fixing that.

--- src/recipes/RecipeAdd.js
+++ src/recipes/RecipeAdd.js
@@ -1,36 +1,58 @@
 import React, { Component } from 'react'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { graphql } from 'react-apollo'
+
+import recipesService from './service'
 
 class RecipeAdd extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: '',
+      description: '',
+      cookTime: '',
+      foods: ''
+    }
+  }
+
+  handleAddRecipe = (evt) => {
+    evt.preventDefault()
+    this.props.mutate({
+      variables: {
+        name: this.state.name,
+        description: this.state.description,
+        cookTime: this.state.cookTime,
+        foods: this.state.foods
+      }
+    })
+  }
+
   render () {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleAddRecipe}>
           <FormGroup>
-            <Label>
-              <Input/>
-            </Label>
+            <Label for="recipe-name">Recipe Name</Label>
+            <Input id="recipe-name" type="text" onChange={(evt) => this.setState({ name: evt.target.value }) }/>
           </FormGroup>
           <FormGroup>
-            <Label>
-              <Input/>
-            </Label>
+            <Label for="recipe-description">Recipe Description</Label>
+            <Input id="recipe-description" type="textarea" onChange={(evt) => this.setState({ description: evt.target.value }) }/>
           </FormGroup>
           <FormGroup>
-            <Label>
-              <Input/>
-            </Label>
+            <Label for="cook-time">CookTime in Minutes</Label>
+            <Input id="cook-time" type="number" onChange={(evt) => this.setState({ cookTime: evt.target.value }) }/>
           </FormGroup>
           <FormGroup>
-            <Label>
-              <Input/>
-            </Label>
+            <Label for="required-foods">Required Foods</Label>
+            <Input id="required-foods" type="text"/>
           </FormGroup>
-          <Button/>
+          <Button color="success">Add Recipe</Button>
         </Form>
       </div>
+
     )
   }
 }
 
-export default RecipeAdd
+export default graphql(recipesService.addRecipe)(RecipeAdd)
--- src/recipes/service.js
+++ src/recipes/service.js
@@ -25,6 +25,14 @@ class recipesService {
       }
     }
   `
+
+  addRecipe = gql`
+    mutation ($name: String!, $description: String!, $cookTime: Int!) {
+      createRecipe (name: $name, description: $description, cookTime: $cookTime, foodIds: [1, 2, 9]) {
+        id
+      }
+    }
+  `
 }
 
 export default new recipesService()
